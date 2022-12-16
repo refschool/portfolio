532083822a4354d11ef1ac912e791ffcaad881fa
John Tchen <johntchen.mos@gmail.com>
2022-12-16 08:01:45 +0100

    Ajout jeu de données via Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+    
+        for($c = 0 ; $c < 3 ; $c++){
+            $contact = new Contact;
+            $contact->setNom("Nom n°$c");
+            $contact->setPrenom("Prenom $c");
+            $contact->setEmail("test$c@text$c.com");
+            $contact->setTelephone("0123456789");
+            $contact->setMessage("Message");
 
+            $manager->persist($contact);
+        }
         $manager->flush();
     }
 }
