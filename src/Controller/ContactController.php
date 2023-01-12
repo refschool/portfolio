47f2e0ce756abc6cbdb13bea25be0fecadf16842
John Tchen <johntchen.mos@gmail.com>
2023-01-12 11:10:47 +0100

    validation tableau associatif

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\Entity;
 use App\Entity\Contact;
-use App\Repository\ContactRepository;
+use Doctrine\ORM\Entity;
 use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactController extends AbstractController
 {
@@ -50,7 +55,35 @@ class ContactController extends AbstractController
      * @Route("/admin/{id}/editerMessageContact", name="messageContactEdit")
      */
     public function editerMessageContact($id, ContactRepository $contactRepository, Request $request,
-    EntityManagerInterface $em){
+    EntityManagerInterface $em, ValidatorInterface $validator){
+        
+        $client = [
+            'nom' => '',
+            'prenom' => 'John',
+            'voiture' => [
+                'marque' => '',
+                'couleur' => 'Noire'
+            ]
+        ];
+
+        $collection = new Collection([
+            'nom' => new NotBlank(['message' => "Le nom ne doit pas être vide"]),
+            'prenom' => [
+                new NotBlank(['message' => "Le prenom ne doit pas être vide"]),
+                new Length(['min' => 3, 'minMessage' => "Le prenom ne doit pas faire moins de 3 caractères"])
+            ],
+            'voiture' => new Collection([
+                'marque' => new NotBlank(['message'=> 'La marque ne doit pas être vide']),
+                'couleur' => new NotBlank(['message'=> 'La couleur ne doit pas être vide']),
+            ])
+        ]);
+        
+        $erreurs  = $validator->validate($client, $collection);
+        //Compte le nombre d'erreurs
+        if ($erreurs->count() > 0 ){
+            dd("Il y a des erreurs", $erreurs);
+        }
+        dd("Tout va bien");
         $message = $contactRepository->find($id);
 
         $form = $this->createForm(ContactType::class, $message);
