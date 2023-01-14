f879540b15d18d8828b5d065659a3a9eb946a0cd
John Tchen <johntchen.mos@gmail.com>
2023-01-14 17:21:14 +0100

    page login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +11,8 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="security_login")
      */
-    public function login()
+    public function login(): Response
     {
-        $form = $this->createForm(LoginType::class);
-
-        return $this->render('security/login.html.twig', [
-            'formView' => $form->createView()
-        ]);
+        return $this->render('security/login.html.twig', []);
     }
 }
