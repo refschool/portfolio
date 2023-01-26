19e6c80f8c0df380e94215e4db791f9c217a504c
John Tchen <johntchen.mos@gmail.com>
2023-01-26 18:37:06 +0100

    envoi mail par mail trap

--- src/EventDispatcher/MessageSuccessEmailSubscriber.php
+++ src/EventDispatcher/MessageSuccessEmailSubscriber.php
@@ -5,15 +5,20 @@ namespace App\EventDispatcher;
 use App\Event\MessageSuccessEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class MessageSuccessEmailSubscriber implements EventSubscriberInterface
 {
 
     protected $logger;
+    protected $mailer;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     public static function getSubscribedEvents()
@@ -22,8 +27,21 @@ class MessageSuccessEmailSubscriber implements EventSubscriberInterface
             'message.success' => 'sendSuccessEmail'
         ];
     }
+
     public function sendSuccessEmail(MessageSuccessEvent $messageEvent)
     {
-        $this->logger->info("Email envoyé à " . $messageEvent->getMessage()->getEmail());
+        $email = new Email();
+        $fromEmail = $messageEvent->getMessage()->getEmail();
+        $nom = $messageEvent->getMessage()->getNom();
+        $prenom = $messageEvent->getMessage()->getPrenom();
+        $email->from(new Address($fromEmail, $nom . ' ' . $prenom))
+            ->to("admin@test.com")
+            ->text("Le service admin a bien reçu votre message")
+            ->subject("Reception du message");
+
+        $this->mailer->send($email);
+
+        $this->logger->info("Email envoyé à " . $fromEmail);
+        //$this->mailer->send()
     }
 }
