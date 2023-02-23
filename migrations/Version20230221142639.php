3ca48daca4097656e16c7d105080d6ecc0939c28
John Tchen <johntchen.mos@gmail.com>
2023-02-23 23:48:00 +0100

    Clean DBB

--- migrations/Version20230221142639.php
+++ migrations/Version20230221142639.php
@@ -21,14 +21,14 @@ final class Version20230221142639 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE product ADD category_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT fk_product_category_id_category FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD12469DE2');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY fk_product_category_id_category');
         $this->addSql('DROP INDEX IDX_D34A04AD12469DE2 ON product');
         $this->addSql('ALTER TABLE product DROP category_id');
     }
